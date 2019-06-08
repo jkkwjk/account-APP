863e1e1bdcf02c57b47a32caae8d4a088ad41b5d
MEYOSHINO <1134774517@qq.com>
2019-06-08 13:07:58 +0800

    push

---
+++ app/src/main/java/com/jkk/finances/Activity/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.jkk.finances.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.jkk.finances.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
