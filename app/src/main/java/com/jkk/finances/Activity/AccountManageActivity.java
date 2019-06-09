a03a134d251d24ae0af1c5b9cc48e0620d955cd3
MEYOSHINO <1134774517@qq.com>
2019-06-09 13:57:42 +0800

    测试开始前2.0

--- app/src/main/java/com/jkk/finances/Activity/AccountManageActivity.java
+++ app/src/main/java/com/jkk/finances/Activity/AccountManageActivity.java
@@ -1,15 +1,30 @@
 package com.jkk.finances.Activity;
 
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.SpannableString;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.jkk.finances.R;
 
 public class AccountManageActivity extends AppCompatActivity {
-
+    private Button buttoninster;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_manage);
+        buttoninster=(Button)findViewById(R.id.button_account_image);
+
     }
+
 }
---
+++ app/src/main/java/com/jkk/finances/Activity/Edit.java
@@ -0,0 +1,49 @@
+package com.jkk.finances.Activity;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.style.ImageSpan;
+import android.util.AttributeSet;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class Edit extends EditText {
+    public Edit(Context context) {
+        super(context);
+    }
+
+    public Edit(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public Edit(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public Edit(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+    public void insertDrawable(int id) {
+        final SpannableString ss = new SpannableString("easy");
+        //得到drawable对象，即所要插入的图片
+        Drawable d = getResources().getDrawable(id);
+        d.setBounds(0, 0, d.getIntrinsicWidth(), d.getIntrinsicHeight());
+        //用这个drawable对象代替字符串easy
+        ImageSpan span = new ImageSpan(d, ImageSpan.ALIGN_BASELINE);
+        //包括0但是不包括"easy".length()即：4。[0,4)。值得注意的是当我们复制这个图片的时候，实际是复制了"easy"这个字符串。
+        ss.setSpan(span, 0, "easy".length(), Spannable.SPAN_INCLUSIVE_EXCLUSIVE);
+        append(ss);
+    }
+    public void insertDrawable(Bitmap b) {
+        final SpannableString ss = new SpannableString("//|||}}");
+        //得到drawable对象，即所要插入的图片
+        ImageSpan span = new ImageSpan(b);
+        //包括0但是不包括"easy".length()即：4。[0,4)。值得注意的是当我们复制这个图片的时候，实际是复制了"easy"这个字符串。
+        ss.setSpan(span, 0, "//|||}}".length(), Spannable.SPAN_INCLUSIVE_EXCLUSIVE);
+        append(ss);
+    }
+
+}
